b1a9fbc8a913d7f7a0988f39d39bea8ec04013ed
macro.dat <macro.dat@trim-inc.com>
2018-05-30 14:10:39 +0900

    Initial commit

---
+++ LoginHelper/LoginHelper.h
@@ -0,0 +1,19 @@
+//
+//  LoginHelper.h
+//  LoginHelper
+//
+//  Created by Do Thanh Dat on 2018/05/30.
+//  Copyright © 2018 DT Dat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoginHelper.
+FOUNDATION_EXPORT double LoginHelperVersionNumber;
+
+//! Project version string for LoginHelper.
+FOUNDATION_EXPORT const unsigned char LoginHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginHelper/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LoginHelper' do
+    pod 'Validator'
+    pod  'FBSDKCoreKit'
+    pod  'FBSDKLoginKit'
+    pod 'Bolts'
+     pod 'Google/SignIn'
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LoginHelper
+
+end
